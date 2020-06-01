858440812ffba5602f574fdea447950793555dc5
amine <amine@lift-school.com>
2020-06-01 20:16:57 +0200

    return to line

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Add the build hosts
 docker buildx create --use --name sauron-build
-docker buildx --append --name sauron-build tcp://192.168.1.21:2375
+docker buildx create --append --name sauron-build tcp://192.168.1.21:2375
+
+# Launch java server
 java -jar /opt/jenkins/jenkins.war --httpPort=7100
