36b880757fb428cefee22c9092a07320a34aca16
amine <amine@lift-school.com>
2020-06-02 11:03:29 +0200

    comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # Add the build hosts
+
+# ARM64 builder
 docker buildx create --use --name sauron-build tcp://192.168.1.32:2375
+
+# AMD64 builder
 docker buildx create --append --name sauron-build tcp://192.168.1.21:2375
 
 # Launch java server
